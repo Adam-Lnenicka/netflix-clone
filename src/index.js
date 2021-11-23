f6fbeb00e4c43a3c260ad04730189f63754baa2c
Adam Lnenicka <adam.lnenicka123@gmail.com>
2021-11-23 12:58:16 +0000

    PR comments resolved ENV not setup properly

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import App from "./App.js";
 ReactDOM.render(<App />, document.getElementById("app"));
 
 applyMiddleware.listen(3000, () => {
-  console.log("app is running in mode: ", process.env.NODE_ENV);
+  console.log("app is running in mode: ", process.env.REACT_APP_BASE_URL);
 });
