7a3f78f9781ef0e557297951e9c54d766f14d142
Adam Lnenicka <adam.lnenicka123@gmail.com>
2021-11-26 10:07:53 +0000

    add movie page

---
+++ src/components/AddMovie.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+
+const AddMovie = () => {
+  const [values, setValues] = useState({
+    title: "",
+    releaseDate: "",
+    movieUrl: "",
+    genre: "",
+    overview: "",
+    runtime: "",
+  });
+
+  const handleTitleChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      title: event.target.value,
+    }));
+  };
+
+  const handleReleaseDateChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      releaseDate: event.target.value,
+    }));
+  };
+
+  const handleMovieUrlChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      movieUrl: event.target.value,
+    }));
+  };
+
+  const handleGenreChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      genre: event.target.value,
+    }));
+  };
+
+  const handleOverviewChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      overview: event.target.value,
+    }));
+  };
+
+  const handleRuntimeChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      runtime: event.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <h1>Add Movie</h1>
+      <form>
+        <label>title</label>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={values.title}
+          setValue={handleTitleChange}
+        />
+
+        <label>release date</label>
+        <input
+          type="text"
+          name="release-date"
+          placeholder="Release Date"
+          value={values.releaseDate}
+          setValue={handleReleaseDateChange}
+        />
+
+        <label>movie url</label>
+        <input
+          type="text"
+          name="movie-url"
+          placeholder="Movie Url Here"
+          value={values.movieUrl}
+          setValue={handleMovieUrlChange}
+        />
+
+        <label>genre</label>
+        <select name="genre" value={values.genre} setValue={handleGenreChange}>
+          <option>Horror</option>
+          <option>Comedy</option>
+        </select>
+
+        <label>overview</label>
+        <input
+          type="text"
+          name="overview"
+          placeholder="Overview"
+          value={values.overview}
+          setValue={handleOverviewChange}
+        />
+
+        <label>runtime</label>
+        <input
+          type="text"
+          name="runtime"
+          placeholder="Runtime"
+          value={values.runtime}
+          setValue={handleRuntimeChange}
+        />
+
+        <button>reset</button>
+        <button>submit</button>
+      </form>
+    </>
+  );
+};
+
+export default AddMovie;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,55 +6,55 @@ import ErrorBoundary from "../components/ErrorBoundary";
 
 const Home = () => {
   const movieData = [
-    // {
-    //   id: 1,
-    //   title: "Two Years at Sea",
-    //   description: "Documentary",
-    //   year: 1993,
-    //   image:
-    //     "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    // },
-    // {
-    //   id: 2,
-    //   title: "Def-Con 4",
-    //   description: "Action|Sci-Fi",
-    //   year: 2010,
-    //   image:
-    //     "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    // },
-    // {
-    //   id: 3,
-    //   title: "Sparrows Dance",
-    //   description: "Drama|Romance",
-    //   year: 2001,
-    //   image:
-    //     "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    // },
-    // {
-    //   id: 4,
-    //   title:
-    //     "Garden of the Finzi-Continis, The (Giardino dei Finzi-Contini, Il)",
-    //   description: "Drama",
-    //   year: 1988,
-    //   image:
-    //     "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    // },
-    // {
-    //   id: 5,
-    //   title: "Zatoichi",
-    //   description: "Action|Drama",
-    //   year: 1993,
-    //   image:
-    //     "https://images.unsplash.com/photo-1637758180067-71d95d566a9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60",
-    // },
-    // {
-    //   id: 6,
-    //   title: "Natural, The",
-    //   description: "Drama",
-    //   year: 1973,
-    //   image:
-    //     "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW92aWV8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
-    // },
+    {
+      id: 1,
+      title: "Two Years at Sea",
+      description: "Documentary",
+      year: 1993,
+      image:
+        "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: 2,
+      title: "Def-Con 4",
+      description: "Action|Sci-Fi",
+      year: 2010,
+      image:
+        "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: 3,
+      title: "Sparrows Dance",
+      description: "Drama|Romance",
+      year: 2001,
+      image:
+        "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: 4,
+      title:
+        "Garden of the Finzi-Continis, The (Giardino dei Finzi-Contini, Il)",
+      description: "Drama",
+      year: 1988,
+      image:
+        "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: 5,
+      title: "Zatoichi",
+      description: "Action|Drama",
+      year: 1993,
+      image:
+        "https://images.unsplash.com/photo-1637758180067-71d95d566a9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: 6,
+      title: "Natural, The",
+      description: "Drama",
+      year: 1973,
+      image:
+        "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW92aWV8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+    },
   ];
   return (
     <>
@@ -67,8 +67,8 @@ const Home = () => {
           </form>
         </div>
       </div>
-      <div className="card-layout">
-        <ErrorBoundary>
+      <ErrorBoundary>
+        <div className="card-layout">
           {movieData.map((movie) => (
             <MovieCard
               key={movie.id}
@@ -78,8 +78,8 @@ const Home = () => {
               year={movie.year}
             />
           ))}
-        </ErrorBoundary>
-      </div>
+        </div>
+      </ErrorBoundary>
 
       <Footer />
     </>
