71df3a31ae40e6a11a0d2d55bb699ad77a9fb3a3
Adam Lnenicka <adam.lnenicka123@gmail.com>
2021-12-21 10:17:58 +0000

    all card detals displaying in redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -164,9 +164,25 @@ const Home = () => {
       </div>
       {/* <div onClick={deletePost(354912)}>test test delete</div> */}
 
-      <div>
+      <div className="card-layout">
         {apiMoviesArray.map((movie) =>
-          movie.map((m) => <Card key={m.id} title={m.title} />)
+          movie.map((m) => (
+            <MovieCard
+              key={m.id}
+              title={m.title}
+              poster_path={m.poster_path}
+              genres={m.genres.map((g) => (
+                <span key={g}>{g} /</span>
+              ))}
+              release_date={m.release_date}
+              function={bannerHandle}
+              movie={movie}
+              broken={(e) =>
+                (e.target.src =
+                  "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80")
+              }
+            />
+          ))
         )}
       </div>
 
