ce41720a021fb58ff770286b7952fe065804a916
Adam Lnenicka <adam.lnenicka123@gmail.com>
2022-01-11 10:48:05 +0000

    cleanup6

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import Footer from "../components/Footer/Footer";
 import InnerBanner from "../components/InnerBanner/InnerBanner";
 import MovieBanner from "../components/InnerBanner/MovieBanner";
 import Navigation from "../components/FilterNavigation/Navigation";
+import ErrorBoundary from "../components/ErrorBoundary";
+
 import { moviesSorted, removeMovie } from "../store/actionCreators";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -15,62 +17,9 @@ const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const FilterIdSelector = useSelector((state) => state.id);
-  const sortCriteriaSelector = useSelector((state) => state.sortCriteria);
   const movieGenreFilterSelector = useSelector((state) => state.filterMovie);
   const apiMoviesArraySelector = useSelector((state) => state.movies);
   const movieArraySearchSelector = useSelector((state) => state.searchFilter);
-
-  const dispatch = useDispatch();
-
-  const sortMovies = (x) => {
-    if (x === "newest-date") {
-      const result = [apiMoviesArraySelector].sort((a, b) => {
-        const dateA = new Date(a.release_date).getYear();
-        const dateB = new Date(b.release_date).getYear();
-        if (dateA < dateB) {
-          return -1;
-        }
-        if (dateA < dateB) {
-          return 1;
-        }
-        return 0;
-      });
-      dispatch(moviesSorted(result));
-      console.log(sortCriteriaSelector);
-      // setMovies(result);
-    }
-    if (x === "oldest-date") {
-      const result = [...movies].sort((a, b) => {
-        const dateA = new Date(a.release_date).getYear();
-        const dateB = new Date(b.release_date).getYear();
-        if (dateA > dateB) {
-          return -1;
-        }
-        if (dateA > dateB) {
-          return 1;
-        }
-        return 0;
-      });
-      dispatch(moviesSorted(result));
-      console.log(sortCriteriaSelector);
-
-      // setMovies(result);
-    }
-    if (x === "title") {
-      const result = [...movies].sort((a, b) => {
-        if (a.title < b.title) {
-          return -1;
-        }
-        if (a.title < b.title) {
-          return 1;
-        }
-        return 0;
-      });
-      dispatch(moviesSorted(result));
-
-      // setMovies(result);
-    }
-  };
 
   const addMovieHandler = (movie) => {
     setMovies((prevMovies) => {
@@ -117,54 +66,55 @@ const Home = () => {
         drama="Drama"
         crime="Crime"
       />
-
-      <div className="card-layout">
-        {apiMoviesArraySelector.map((movie) =>
-          movie
-            .filter((data) => {
-              if (movieArraySearchSelector === "") {
-                return data;
-              } else if (
-                data.title
-                  .toLowerCase()
-                  .includes(movieArraySearchSelector.toLowerCase())
-              ) {
-                return data;
-              }
-              return null;
-            })
-            .filter((data) => {
-              if (movieGenreFilterSelector === "") {
-                return data;
-              } else if (data.genres.includes(movieGenreFilterSelector)) {
-                return data;
-              }
-              return null;
-            })
-
-            .slice(0, 10)
-            .map((m) => (
-              <MovieCard
-                key={m.id}
-                id={m.id}
-                title={m.title}
-                poster_path={m.poster_path}
-                genres={m.genres.map((g) => (
-                  <span key={g}>{g} /</span>
-                ))}
-                release_date={m.release_date}
-                vote_average={m.vote_average}
-                overview={m.overview}
-                movie={movie}
-                runtime={m.runtime}
-                broken={(e) =>
-                  (e.target.src =
-                    "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80")
+      <ErrorBoundary>
+        <div className="card-layout">
+          {apiMoviesArraySelector.map((movie) =>
+            movie
+              .filter((data) => {
+                if (movieArraySearchSelector === "") {
+                  return data;
+                } else if (
+                  data.title
+                    .toLowerCase()
+                    .includes(movieArraySearchSelector.toLowerCase())
+                ) {
+                  return data;
                 }
-              />
-            ))
-        )}
-      </div>
+                return null;
+              })
+              .filter((data) => {
+                if (movieGenreFilterSelector === "") {
+                  return data;
+                } else if (data.genres.includes(movieGenreFilterSelector)) {
+                  return data;
+                }
+                return null;
+              })
+
+              .slice(0, 10)
+              .map((m) => (
+                <MovieCard
+                  key={m.id}
+                  id={m.id}
+                  title={m.title}
+                  poster_path={m.poster_path}
+                  genres={m.genres.map((g) => (
+                    <span key={g}>{g} /</span>
+                  ))}
+                  release_date={m.release_date}
+                  vote_average={m.vote_average}
+                  overview={m.overview}
+                  movie={movie}
+                  runtime={m.runtime}
+                  broken={(e) =>
+                    (e.target.src =
+                      "https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80")
+                  }
+                />
+              ))
+          )}
+        </div>
+      </ErrorBoundary>
 
       <Footer />
     </>
