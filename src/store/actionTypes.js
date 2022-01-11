301ac37f806e91fef6e0cceb801d35bad722e996
Adam Lnenicka <adam.lnenicka123@gmail.com>
2022-01-11 10:21:17 +0000

    cleanup3

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -3,7 +3,6 @@ export const FILTER_MOVIE = "FILTER_MOVIE";
 export const MOVIE_ARRAY = "MOVIE_ARRAY";
 export const SORT_CRITERIA = "SORT_CRITERIA";
 export const VISIBLE = "VISIBLE";
-export const API = "API";
 export const LOAD_MOVIES = "LOAD_MOVIES";
 export const FILTER_ID = "FILTER_ID";
 export const MOVIES_SORTED = "MOVIES_SORTED";
