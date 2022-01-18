15b907489307a996cb3b0bb37029f44f89ea6cbb
Adam Lnenicka <adam.lnenicka123@gmail.com>
2022-01-18 15:55:56 +0000

    location update

--- src/MoviesLocation.js
+++ src/MoviesLocation.js
@@ -4,26 +4,30 @@ import { useLocation } from "react-router-dom";
 import MoviesListLocation from "./components/MoviesListLocation";
 
 const movies = [
-  { title: "harry potter", id: 1, genre: "fantasy", rating: 35 },
-  { title: "skyfall", id: 2, genre: "horror", rating: 24 },
-  { title: "black panther", id: 3, genre: "comedy", rating: 18 },
+  { title: "harry potter", id: 1, genre: "fantasy", vote_average: 1 },
+  { title: "skyfall", id: 2, genre: "horror", vote_average: 8 },
+  { title: "black panther", id: 3, genre: "comedy", vote_average: 7 },
 ];
 
 const MoviesLocation = () => {
   const location = useLocation();
+  const apiMoviesArray = useApiMoviesArraySelector();
 
   const query = new URLSearchParams(location.search);
   const genre = query.get("");
-  const rating = query.get("rating");
-  let filteredMovies = genre ? movies.filter((u) => u.genre === genre) : movies;
-  filteredMovies = rating
-    ? filteredMovies.filter((u) => u.rating >= rating)
-    : filteredMovies;
+  const id = 2;
+  let filteredMovies = id
+    ? apiMoviesArray.map((m) => m.filter((u) => u.id >= id))
+    : apiMoviesArray;
+  // filteredMovies = vote_average
+  //   ? filteredMovies.filter((u) => u.vote_average >= vote_average)
+  //   : filteredMovies;
 
   return (
     <>
       <h2>movies</h2>
       <MoviesListLocation movies={filteredMovies}></MoviesListLocation>
+      {console.log("location test" + filteredMovies)}
     </>
   );
 };
