f010c5411079509bb6b4b4d07d8caec443d7ea7e
Adam Lnenicka <adam.lnenicka123@gmail.com>
2022-02-16 09:23:47 +0000

    thunk 100% coverage

--- src/store/thunk.js
+++ src/store/thunk.js
@@ -31,39 +31,62 @@ export const loadMoviesThunk = () => (dispatch) =>
   axios.get("http://localhost:4000/movies").then((moviesData) =>
     dispatch({
       type: MOVIES_LOADED,
-      payload: moviesData.data,
+      payload: moviesData.data.data,
     })
   );
 
-export const loadMoviesByTitleThunk = () => async (dispatch) => {
-  const apiLink = `${api}?sortOrder=desc&sortBy=title`;
-  const apiData = await fetch(apiLink);
-  const moviesData = await apiData.json();
-  dispatch({
-    type: MOVIES_LOADED_BY_TITLE,
-    payload: moviesData.data,
-  });
-};
+export const loadMoviesByTitleThunk = () => (dispatch) =>
+  axios.get(`${api}?sortOrder=desc&sortBy=title`).then((moviesData) =>
+    dispatch({
+      type: MOVIES_LOADED_BY_TITLE,
+      payload: moviesData.data.data,
+    })
+  );
 
-export const loadMoviesByRatingThunk = () => async (dispatch) => {
-  const apiLink = `${api}?sortOrder=desc&sortBy=vote_average`;
-  const apiData = await fetch(apiLink);
-  const moviesData = await apiData.json();
-  dispatch({
-    type: MOVIES_LOADED_BY_RATING,
-    payload: moviesData.data,
-  });
-};
+export const loadMoviesByRatingThunk = () => (dispatch) =>
+  axios.get(`${api}?sortOrder=desc&sortBy=vote_average`).then((moviesData) =>
+    dispatch({
+      type: MOVIES_LOADED_BY_RATING,
+      payload: moviesData.data.data,
+    })
+  );
+export const loadMoviesByDateThunk = () => (dispatch) =>
+  axios.get(`${api}?sortOrder=desc&sortBy=release_date`).then((moviesData) =>
+    dispatch({
+      type: MOVIES_LOADED_BY_DATE,
+      payload: moviesData.data.data,
+    })
+  );
 
-export const loadMoviesByDateThunk = () => async (dispatch) => {
-  const apiLink = `${api}?sortOrder=desc&sortBy=release_date`;
-  const apiData = await fetch(apiLink);
-  const moviesData = await apiData.json();
-  dispatch({
-    type: MOVIES_LOADED_BY_DATE,
-    payload: moviesData.data.data,
-  });
-};
+// export const loadMoviesByTitleThunk = () => async (dispatch) => {
+//   const apiLink = `${api}?sortOrder=desc&sortBy=title`;
+//   const apiData = await fetch(apiLink);
+//   const moviesData = await apiData.json();
+//   dispatch({
+//     type: MOVIES_LOADED_BY_TITLE,
+//     payload: moviesData.data,
+//   });
+// };
+
+// export const loadMoviesByRatingThunk = () => async (dispatch) => {
+//   const apiLink = `${api}?sortOrder=desc&sortBy=vote_average`;
+//   const apiData = await fetch(apiLink);
+//   const moviesData = await apiData.json();
+//   dispatch({
+//     type: MOVIES_LOADED_BY_RATING,
+//     payload: moviesData.data,
+//   });
+// };
+
+// export const loadMoviesByDateThunk = () => async (dispatch) => {
+//   const apiLink = `${api}?sortOrder=desc&sortBy=release_date`;
+//   const apiData = await fetch(apiLink);
+//   const moviesData = await apiData.json();
+//   dispatch({
+//     type: MOVIES_LOADED_BY_DATE,
+//     payload: moviesData.data.data,
+//   });
+// };
 
 // export const loadFilteredMoviesThunk = () => async (dispatch) => {
 //   const apiLink = `${api}?filter=${filter}`;
--- src/store/thunk.test.js
+++ src/store/thunk.test.js
@@ -2,8 +2,18 @@ import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import moxios from "moxios";
 
-import { MOVIES_LOADED } from "./actionTypes";
-import { loadMoviesThunk } from "./thunk";
+import {
+  MOVIES_LOADED,
+  MOVIES_LOADED_BY_DATE,
+  MOVIES_LOADED_BY_RATING,
+  MOVIES_LOADED_BY_TITLE,
+} from "./actionTypes";
+import {
+  loadMoviesByDateThunk,
+  loadMoviesByRatingThunk,
+  loadMoviesByTitleThunk,
+  loadMoviesThunk,
+} from "./thunk";
 
 describe("SearchMovie actions", () => {
   const middlewares = [thunk];
@@ -39,25 +49,25 @@ describe("SearchMovie actions", () => {
   });
 
   it("Display loaded movies by title", () => {
-    const loaded = [{ type: MOVIES_LOADED, payload: {} }];
+    const loaded = [{ type: MOVIES_LOADED_BY_TITLE, payload: {} }];
 
-    return store.dispatch(loadMoviesThunk()).then(() => {
+    return store.dispatch(loadMoviesByTitleThunk()).then(() => {
       expect(store.getActions()).toEqual(loaded);
     });
   });
 
   it("Display loaded movies by date", () => {
-    const loaded = [{ type: MOVIES_LOADED, payload: {} }];
+    const loaded = [{ type: MOVIES_LOADED_BY_DATE, payload: {} }];
 
-    return store.dispatch(loadMoviesThunk()).then(() => {
+    return store.dispatch(loadMoviesByDateThunk()).then(() => {
       expect(store.getActions()).toEqual(loaded);
     });
   });
 
   it("Display loaded movies by rating", () => {
-    const loaded = [{ type: MOVIES_LOADED, payload: {} }];
+    const loaded = [{ type: MOVIES_LOADED_BY_RATING, payload: {} }];
 
-    return store.dispatch(loadMoviesThunk()).then(() => {
+    return store.dispatch(loadMoviesByRatingThunk()).then(() => {
       expect(store.getActions()).toEqual(loaded);
     });
   });
