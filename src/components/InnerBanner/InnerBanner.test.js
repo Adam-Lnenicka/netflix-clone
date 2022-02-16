110a9a58a428e2cf1d6139732a3a6b743bb014ad
Adam Lnenicka <adam.lnenicka123@gmail.com>
2022-02-16 15:24:13 +0000

    commit all

--- src/components/InnerBanner/InnerBanner.test.js
+++ src/components/InnerBanner/InnerBanner.test.js
@@ -4,61 +4,36 @@ import React from "react";
 import toJSON from "enzyme-to-json";
 import { mount, shallow } from "enzyme";
 import InnerBanner from "./InnerBanner";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
 
-describe("Inner Banner", () => {
-  const initialState = {};
-  const mockStore = configureStore();
+describe("Inner Bannner shallow wrapper", () => {
+  it("Inner Banner matches snapshot", () => {
+    const wrapper = shallow(<InnerBanner />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
+
+describe("Inner Banner mount wrapper", () => {
   const mockFunction = jest.fn();
 
-  it("Simulates dispatch on search function", () => {
-    const store = mockStore(initialState);
+  let wrapper;
 
-    const wrapper = mount(
-      // <Provider store={store}>
-      <InnerBanner onCLick={mockFunction} />
-      // </Provider>
-    );
+  beforeEach(() => {
+    wrapper = mount(<InnerBanner onCLick={mockFunction} />);
+  });
+
+  it("Simulates dispatch on search function", () => {
     wrapper.find("button").simulate("click");
     expect(mockFunction).toBeCalled();
   });
 
   it("submit button displays", () => {
-    // const store = mockStore(initialState);
-
-    const wrapper = mount(
-      // <Provider store={store}>
-      <InnerBanner />
-      // </Provider>
-    );
-
     const submitButton = wrapper.find(".button-main");
     expect(submitButton.exists()).toBe(true);
-    // });
   });
 
   it("state updates with value of input box upon change", () => {
-    const wrapper = mount(
-      // <Provider store={store}>
-      <InnerBanner />
-      // </Provider>
-    );
-
     const inputBox = wrapper.find(".input-box");
     const mockEvent = { target: { value: "input" } };
-
     inputBox.simulate("change", mockEvent);
-  });
-
-  it("matches snapshot", () => {
-    // const store = mockStore(initialState);
-
-    const wrapper = shallow(
-      // <Provider store={store}>u
-      <InnerBanner />
-      // </Provider>
-    );
-    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
